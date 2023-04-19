ec1ab562e8de52b244d7c6815864a01a3d893533
Huncho Jack <meniman98@gmail.com>
2023-04-19 17:14:01 +0200

    DalleService is done

--- src/app/services/dalle.service.ts
+++ src/app/services/dalle.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DalleService {
+  private baseUrl = "https://api.openai.com/v1/images/generations"
 
   constructor() { }
+
+  async generateImage(prompt: string, apiKey: string): Promise<any> {
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer ${apiKey}",
+    };
+
+    const data = {
+      "prompt": prompt,
+      "n": 1,
+    };
+
+    try {
+      const response = await axios.post(this.baseUrl, data, {headers: headers});
+      return response.data;
+    } catch(error) {
+      console.error(error);
+    }
+  }
 }
